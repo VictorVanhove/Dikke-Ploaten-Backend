00d7a778a0189dd23a3c430873999c87a2062d1f
Victor Vanhove <victorvanhove@hotmail.com>
2019-12-30 19:38:13 +0100

    Reverted project

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use('/',index);
 app.use('/api/user',user);
-app.use('/api/albums', db.json)
 app.use('/api/task',task);
 
 //Database connection
